2143dd2693b2d113ab35fb60b8bb22670f41c81a
Andovip <94870876+Andovip@users.noreply.github.com>
2021-11-22 19:26:26 -0300

    Create dio.py
    Esse é o Primeiro desafio da aula de Lógica, pelo PyCharm Os Acentos Funcionam. Usando o Visual Saem Bugados, Ainda estou em faze de aprendizagem, então tenho muito a aprender.

---
+++ dio.py
@@ -0,0 +1,17 @@
+print('Desafio aula 1 Lógica.')
+# Nota 1 + Nota 2 + Nota 3 + Nota 4;
+# Somar Todas as Notas;
+nota1= 5
+nota2= 7
+nota3= 10
+nota4= 3
+soma=(nota1+nota2+nota3+nota4)
+print('Nota1 =  {} + Nota2 = {} + Nota2 = {} + Nota4 = {} = {} '.format(nota1, nota2, nota3, nota4, soma))
+# Dividir o numero de notas por 4 que é o total de notas informadas.
+média=soma/4
+print(' a Média das Notas Requeridas é: {} '.format(média))
+# estou aprendendo ainda esta linguagem de programação.
+# Ja Havia feito este curso de logica No IFSP Campus itapetininga
+# Más para provar que estou disposto a aprender não importa maneira
+# Digital ou presencial. acredito que preciso mostrar disposição.
+# e capricho.
